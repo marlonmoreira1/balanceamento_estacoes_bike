dab93cb87f8ce10c6286b032124d4aa47f17efd2
marlonmoreira1 <71144665+marlonmoreira1@users.noreply.github.com>
2024-12-10 20:23:03 -0300

    Esqueci de levar no outro commit, mas é a função que faz a pilha funcionar e alertar somente as novas estações.

---
+++ alertas/update_alerts.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def get_new_stations(novos_dados, historico_requisicoes):    
+    
+    if historico_requisicoes:
+        ultimo_df = historico_requisicoes[-1]  
+        ultimo_ids = set(ultimo_df['station_id'])  
+    else:
+        ultimo_ids = set()
+    
+    novos_ids = set(novos_dados['station_id'])
+    novas_estacoes_ids = novos_ids - ultimo_ids
+    
+    novas_estacoes_df = novos_dados[novos_dados['station_id'].isin(novas_estacoes_ids)]
+    historico_requisicoes.append(novos_dados)
+
+    return novas_estacoes_df
