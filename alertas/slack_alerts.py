547c50a247738ac5d89f6039a5cc8e9f396df34b
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-12-19 22:40:33 -0300

    Update slack_alerts.py

--- alertas/slack_alerts.py
+++ alertas/slack_alerts.py
@@ -4,7 +4,7 @@ from slack_sdk import WebClient
 from tabulate import tabulate
 
 def get_message(message):
-    client = WebClient(token=st.secrets["SLACK_TOKEN"])
+    client = WebClient(token=os.environ["SLACK_TOKEN"])
 
     client.chat_postMessage(
         channel="alertas-de-vazias", 
