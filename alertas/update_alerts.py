aa447afc5bd6d3404ff08852a698632e1101fe85
marlonmoreira1 <71144665+marlonmoreira1@users.noreply.github.com>
2024-12-20 11:37:22 -0300

    Fix do alertas com a concatenação de todos os dfs da pilha.

--- alertas/update_alerts.py
+++ alertas/update_alerts.py
@@ -3,8 +3,8 @@ import pandas as pd
 def get_new_stations(novos_dados, historico_requisicoes):    
     
     if historico_requisicoes:
-        ultimo_df = historico_requisicoes  
-        ultimo_ids = set(ultimo_df['station_id'])  
+        combinado_df = pd.concat(historico_requisicoes, ignore_index=True)   
+        ultimo_ids = set(combinado_df['station_id'])  
     else:
         ultimo_ids = set()
     
