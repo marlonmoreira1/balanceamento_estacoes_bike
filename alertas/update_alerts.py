683727651b833f2b77858acd4b5105be38d81471
marlonmoreira1 <71144665+marlonmoreira1@users.noreply.github.com>
2024-12-20 23:56:24 -0300

    Usando o id errado tbm.

--- alertas/update_alerts.py
+++ alertas/update_alerts.py
@@ -5,14 +5,14 @@ def get_new_stations(novos_dados):
     
     if st.session_state.historico_requisicoes:
         combinado_df = pd.concat(st.session_state.historico_requisicoes, ignore_index=True)   
-        ultimo_ids = set(combinado_df['station_id'])  
+        ultimo_ids = set(combinado_df['new_id'])  
     else:
         ultimo_ids = set()
     
-    novos_ids = set(novos_dados['station_id'])
+    novos_ids = set(novos_dados['new_id'])
     novas_estacoes_ids = novos_ids - ultimo_ids
     
-    novas_estacoes_df = novos_dados[novos_dados['station_id'].isin(novas_estacoes_ids)]
+    novas_estacoes_df = novos_dados[novos_dados['new_id'].isin(novas_estacoes_ids)]
         
     
     return novas_estacoes_df
